6c86f83afa556f206afb52d1ab1008af21fc3ea0
lucastomazcs <lucastomaz023@gmail.com>
2024-06-25 15:48:14 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from player import Player
+
 
 pygame.init()
 
@@ -17,16 +19,26 @@ def main():
     clock = pygame.time.Clock()
     rodando = True
 
+    #Criando o objeto jogador:
+    jogador = Player(posicaobomba=(0,0), tempo=(0), raiodeexplosao=(2), posicao=(90,100), vida= (100), velocidade=(5), range_bomba=(10))
+    todos_os_sprites = pygame.sprite.Group()
+    todos_os_sprites.add(jogador)
+
+
     while rodando:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                rodando = False
 
+        todos_os_sprites.update()
+
         tela.fill(branco)
+        todos_os_sprites.draw(tela)
         
+
         pygame.display.flip()
         clock.tick(60)
-    
+
     pygame.quit()
     sys.exit()
 
--- player.py
+++ player.py
@@ -1,14 +1,22 @@
+import pygame
 from bomba import Bomba
 
-class Player(Bomba):
+class Player(Bomba, pygame.sprite.Sprite):
    def __init__(self, posicaobomba, tempo, raiodeexplosao, posicao, vida, velocidade, range_bomba):
       super().__init__(posicaobomba, tempo, raiodeexplosao)
+      pygame.sprite.Sprite.__init__(self) #Inicializa uma superclasse do pygame
+
       self.__posicao = posicao
       self.__vida = vida
       self.__velocidade = velocidade
       self.__range_bomba = range_bomba
-    
-    
+
+      self.image = pygame.Surface((40,50))
+      self.image.fill((255,0,0))
+
+      self.rect = self.image.get_rect(topleft=posicao)
+
+
    @property
    def posicao(self):
         return self.__posicao
@@ -21,3 +29,19 @@ class Player(Bomba):
    @property
    def raio(self):
         return self.__range_bomba
+
+   def movimento(self):
+     keys = pygame.key.get_pressed()
+     if keys[pygame.K_w]:
+         self.rect.y -= self.__velocidade
+     if keys[pygame.K_s]:
+         self.rect.y += self.__velocidade
+     if keys[pygame.K_d]:
+         self.rect.x += self.__velocidade
+     if keys[pygame.K_a]:
+         self.rect.x -= self.__velocidade
+
+     self.__posicao = self.rect.topleft
+
+   def update(self):
+     self.movimento() 
