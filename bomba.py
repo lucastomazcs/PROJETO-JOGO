42d16416c12545311e78cf1f396c24bc2d054710
lucastomazcs <lucastomaz023@gmail.com>
2024-06-25 11:38:08 -0300

    g

--- bomba.py
+++ bomba.py
@@ -1,2 +1,18 @@
 class Bomba:
-    pass
+    
+    def __init__(self, posicaobomba, tempo, raiodeexplosao) -> None:
+        self.__posicaobomba = posicaobomba
+        self.__tempo = tempo
+        self.__raiodeexplosao = raiodeexplosao
+
+    @property
+    def posicaoBomba(self):
+        return self.__posicaobomba
+    @property
+    def tempo(self):
+        return self.__tempo
+    @property
+    def raio(self):
+        return self.__raiodeexplosao
+    
+
--- player.py
+++ player.py
@@ -1,2 +1,23 @@
-class Player:
-    pass
+from bomba import Bomba
+
+class Player(Bomba):
+   def __init__(self, posicaobomba, tempo, raiodeexplosao, posicao, vida, velocidade, range_bomba) -> None:
+      super().__init__(posicaobomba, tempo, raiodeexplosao)
+      self.__posicao = posicao
+      self.__vida = vida
+      self.__velocidade = velocidade
+      self.__range_bomba = range_bomba
+    
+    
+   @property
+   def posicao(self):
+        return self.__posicao
+   @property
+   def vida(self):
+        return self.__vida
+   @property
+   def velocidade(self):
+        return self.__velocidade
+   @property
+   def raio(self):
+        return self.__range_bomba
