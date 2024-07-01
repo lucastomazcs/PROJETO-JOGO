1acb2f67440ea99dac994346de835d685c5f6db7
lucastomazcs <lucastomaz023@gmail.com>
2024-07-01 14:22:43 -0300

    inimigo

--- inimigo.py
+++ inimigo.py
@@ -1,2 +1,23 @@
 class Inimigo:
-    pass
+    
+    def __init__(self, posicao, vida, velocidade, direcao):
+        self.__posicao = posicao
+        self.__vida = vida
+        self.__velocidade = velocidade
+        self.__direcao = direcao
+    
+    @property
+    def posicao(self):
+        return self.__posicao
+    @property
+    def vida(self):
+        return self.__vida
+    @property
+    def velocidade(self):
+        return self.__velocidade
+    @property
+    def direcao(self):
+        return self.__direcao
+
+    def movimento(self):
+        pass
