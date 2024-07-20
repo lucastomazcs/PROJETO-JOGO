5496575876bfc91b07e0eeb46eb6916792731c58
Sofia Castilho Latini Gonçalves <sofia.goncalves@ufv.br>
2024-07-20 20:50:14 -0300

    movimentação inimigo
    testando

--- inimigo.py
+++ inimigo.py
@@ -1,8 +1,10 @@
 from pygame.sprite import Sprite
 import pygame
 import math
-from mapa import Mapa
 import random
+from mapa import Mapa
+from bomba import Bomba
+from player import Player
 
 
 class Inimigo(Sprite):
@@ -47,7 +49,7 @@ class Inimigo(Sprite):
         self.rect.bottomright = posicao
 
         # Tempo de troca de animação:
-        self.tempo_animacao = 0.1
+        self.tempo_animacao = 1
         self.contador_tempo = 0
 
     @property
@@ -66,37 +68,60 @@ class Inimigo(Sprite):
     def direcao(self):
         return self.__direcao
 
-    #não está funcionando ainda
-    def movimento(self, posicao_jogador, dt: float):
-        # Calcula a direção para o jogador:
-        jogador_x, jogador_y = posicao_jogador
-        inimigo_x, inimigo_y = self.rect.bottomright
-        direcao_x = jogador_x - inimigo_x
-        direcao_y = jogador_y - inimigo_y
-        distancia = math.hypot(direcao_x, direcao_y)
+    def movimento(self, posicao_player, dt: float):
+        # Calcula a direção para o jogador
+        direcao_x, direcao_y = 0, 0
+        if self.rect.x < posicao_player[0]:
+            direcao_x = 1
+        elif self.rect.x > posicao_player[0]:
+            direcao_x = -1
+        if self.rect.y < posicao_player[1]:
+            direcao_y = 1
+        elif self.rect.y > posicao_player[1]:
+            direcao_y = -1
 
-        if distancia > 0:
-            direcao_x /= distancia
-            direcao_y /= distancia
+        # Movimenta o inimigo no eixo x
+        self.rect.x += direcao_x * self.velocidade * dt
 
-        # Atualizando posição inimigo:
-        self.rectx += direcao_x * self.velocidade * dt
-        self.recty += direcao_y * self.velocidade * dt
+        # Checa colisões com blocos e bombas no eixo X
+        if pygame.sprite.spritecollideany(self, self.mapa.blocos) or pygame.sprite.spritecollideany(self, self.mapa.bombas):
+            # Reverte movimento no eixo X
+            self.rect.x -= direcao_x * self.velocidade * dt
 
-        # Atualiza posição interna:
-        self.__posicao = (self.rect.x, self.rect.y)
+        # Movimenta o inimigo no eixo Y
+        self.rect.y += direcao_y * self.velocidade * dt
+        
+        # Checa colisões com blocos e bombas no eixo Y
+        if pygame.sprite.spritecollideany(self, self.mapa.blocos) or pygame.sprite.spritecollideany(self, self.mapa.bombas):
+            # Reverte movimento no eixo Y
+            self.rect.y -= direcao_y * self.velocidade * dt
 
-        # Adicionar colisão com obstaculos:
+        # Atualiza a posição interna do inimigo
+        self.__posicao = self.rect.topleft
 
-    ''' def animacao(self, dt: float):
-            self.contador_tempo += dt
-            if self.contador_tempo >= self.tempo_animacao:
-                self.contador_tempo = 0
-                self.image_index = (self.image_index + 1) % len(self.images)
-                self.image = self.images[self.image_index]
+    def colisao(self, sprite, eixo):
+        if eixo == 'x':
+            if self.rect.x < sprite.rect.x:
+                self.rect.right = sprite.rect.left
+            else:
+                self.rect.left = sprite.rect.right
+        elif eixo == 'y':
+            if self.rect.y < sprite.rect.y:
+                self.rect.bottom = sprite.rect.top
+            else:
+                self.rect.top = sprite.rect.bottom
 
-        def update(self, posicao_jogador, dt: float):
-            posicao_antiga = self.rect.bottomright
-        # Atualiza animação quando inimigo se move:
-            if self.rect.topleft != posicao_antiga:
-                self.animacao(dt)''' #para parar de se mover aleatoriamente
+    def animacao(self, dt: float):
+        self.contador_tempo += dt
+        if self.contador_tempo >= self.tempo_animacao:
+            self.contador_tempo = 0
+            self.image_index = (self.image_index + 1) % len(self.images)
+            self.image = self.images[self.image_index]
+
+    def update(self, posicao_player, dt: float):
+        #print(f"Update chamado com posicao_player: {posicao_player}, dt: {dt}") #Debug
+        #Atualiza a posição do inimigo
+        self.movimento(posicao_player, dt)
+        #Atualiza animação quando inimigo se move:
+        self.animacao(dt)
+        print(self.velocidade) #Debug
