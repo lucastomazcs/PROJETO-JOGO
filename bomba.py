6a574c7b255c3823975a2cb63f5ad3f8341d82b8
lucastomazcs <lucastomaz023@gmail.com>
2024-07-22 07:51:36 -0300

    Reversão da bomba

--- bomba.py
+++ bomba.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Sprite, Group
 
 class Explosão(Sprite):
+    
     def __init__(self, posicao, tamanho, tempo_animacao, mapa) -> None:
         super().__init__()
 
@@ -9,14 +10,16 @@ class Explosão(Sprite):
             pygame.transform.scale(pygame.image.load('Explosão/explosao.png').convert_alpha(), tamanho),
             pygame.transform.scale(pygame.image.load('Explosão/explosao2.png').convert_alpha(), tamanho),
             pygame.transform.scale(pygame.image.load('Explosão/explosao3.png').convert_alpha(), tamanho)
+
         ]
 
         self.image_index = 0
         self.image = self.images[self.image_index]
-        self.rect = self.image.get_rect(center=posicao)
+        self.rect = self.image.get_rect(center = posicao)
         self.tempo_animacao = tempo_animacao
         self.contador_tempo = 0
         self.mapa = mapa
+
 
     def update(self, dt):
         self.contador_tempo += dt
@@ -29,13 +32,13 @@ class Explosão(Sprite):
                 self.kill()
 
 class Bomba(Sprite):
-    def __init__(self, posicaobomba, tempo, raiodeexplosao, tamanho, direcao, mapa):
+    
+    def __init__(self, posicaobomba, tempo, raiodeexplosao, tamanho, mapa):
         pygame.sprite.Sprite.__init__(self)
 
         self.__posicaobomba = posicaobomba
         self.__tempo = tempo
         self.__raiodeexplosao = raiodeexplosao
-        self.direcao = direcao  # Adiciona a direção como atributo
         self.tempo_decorrido = 0
         self.mapa = mapa
 
@@ -44,65 +47,54 @@ class Bomba(Sprite):
             pygame.transform.scale(pygame.image.load('Bombas/bombinha1.png').convert_alpha(), tamanho),
             pygame.transform.scale(pygame.image.load('Bombas/bombinha02.png').convert_alpha(), tamanho),
             pygame.transform.scale(pygame.image.load('Bombas/bombinha03.png').convert_alpha(), tamanho),
-            pygame.transform.scale(pygame.image.load('Bombas/bombinha04.png').convert_alpha(), tamanho)
+            pygame.transform.scale(pygame.image.load('Bombas/bombinha04.png').convert_alpha(), tamanho) #Provisorio, porra
         ]
 
         self.image = self.images[self.image_index]
-        self.rect = self.image.get_rect(topleft=posicaobomba)
+        self.rect = self.image.get_rect(topleft = posicaobomba)
         self.tempo_animacao = 0.3
         self.contador_tempo = 0
-
+    
     def criar_explosao(self): 
         explosao = Explosão(self.posicaoBomba, (self.__raiodeexplosao * 5, self.__raiodeexplosao * 5), 0.3, self.mapa)
+
         self.mapa.explosoes.add(explosao)
+
         return explosao
-
-    def calcular_bloco_mirado(self):
-        # Calcule a posição do bloco com base na direção
-        x, y = self.posicaoBomba
-        if self.direcao == 'up':
-            y -= self.__raiodeexplosao
-        elif self.direcao == 'down':
-            y += self.__raiodeexplosao
-        elif self.direcao == 'left':
-            x -= self.__raiodeexplosao
-        elif self.direcao == 'right':
-            x += self.__raiodeexplosao
-        return x, y
-
+    
     def causar_dano(self, explosao):
-        bloco_mirado_pos = self.calcular_bloco_mirado()
+        raio_explosao = explosao.rect
         bloco_destruido = False
+        inimigo_atingido = set()
 
         for bloco in self.mapa.blocos:
-            if bloco.rect.collidepoint(bloco_mirado_pos) and bloco.destrutivel:
-                print(f"Colisão detectada com bloco destrutível: {bloco.rect}")
+            if raio_explosao.colliderect(bloco.rect) and bloco.destrutivel:
+                print(f"Colisão detectada com bloco destrutível: {bloco.rect}") #Testando a colisão
                 bloco.kill()
                 self.mapa.blocos.remove(bloco)
                 bloco_destruido = True
                 break
-
-        # Checa se jogadores estão dentro do raio de explosão
-        raio_explosao = explosao.rect
+                
         for jogador in self.mapa.jogadores:
             if raio_explosao.colliderect(jogador.rect):
                 print("Colisão com jogador detectada")
                 jogador.morrer()
 
-        # Checa se inimigos estão dentro do raio de explosão
         for inimigo in self.mapa.inimigos:
             if raio_explosao.colliderect(inimigo.rect):
-                print("Colisão com inimigo detectada")
+                print("Colisão com jogador detectada")
                 inimigo.sofrer_dano()
-
+                
+    
     def explodir(self):
-        explosao = self.criar_explosao()
+        explosao= self.criar_explosao()
         self.causar_dano(explosao)
         self.kill()
 
+
     def update(self, dt):
         dt = dt / 10
-        self.tempo_decorrido += dt
+        self.tempo_decorrido +=  dt
         self.contador_tempo += dt
         if self.contador_tempo >= self.tempo_animacao:
             self.contador_tempo = 0
@@ -111,6 +103,7 @@ class Bomba(Sprite):
         if self.tempo_decorrido >= self.__tempo:
             self.explodir()
 
+   
     @property
     def posicaoBomba(self):
         return self.__posicaobomba
@@ -120,3 +113,6 @@ class Bomba(Sprite):
     @property
     def raio(self):
         return self.__raiodeexplosao
+    
+
+
--- player.py
+++ player.py
@@ -4,143 +4,146 @@ from pygame.sprite import Sprite
 from bomba import Bomba
 
 class Player(Sprite):
-    def __init__(self, posicao, vida, velocidade, range_bomba, mapa, tamanho):
-        super().__init__() #Inicialização da Super classe: Sprite através de um metodo construtor
+   def __init__(self, posicao, vida, velocidade, range_bomba, mapa, tamanho):
+      super().__init__() #Inicialização da Super classe: Sprite através de um metodo construtor
     
-        self.__posicao = posicao
-        self.__vida = vida
-        self.__velocidade = velocidade
-        self.__range_bomba = range_bomba
-        self.mapa = mapa
+      self.__posicao = posicao
+      self.__vida = vida
+      self.__velocidade = velocidade
+      self.__range_bomba = range_bomba
+      self.mapa = mapa
 
-        #Carregando imagens de animação do jogador:
-        #Falta o sprite virando para a esquerda
-        self.images = [
-            pygame.transform.scale(pygame.image.load('Bomberman/bomberman01.png').convert_alpha(), tamanho),
-            pygame.transform.scale(pygame.image.load('Bomberman/bomberman02.png').convert_alpha(), tamanho),
-            pygame.transform.scale(pygame.image.load('Bomberman/bomberman03.png').convert_alpha(), tamanho),
-            pygame.transform.scale(pygame.image.load('Bomberman/bomberman03.png').convert_alpha(), tamanho)
-        ]
-        self.image_index = 0
-        self.image = self.images[self.image_index]
-        self.rect = self.image.get_rect()
+      #Carregando imagens de animação do jogador:
+      #Falta o sprite virando para a esquerda
+      self.images = [
 
-        #Definindo posição inicial do jogador:
-        self.rect.topleft = posicao
+          pygame.transform.scale(pygame.image.load('Bomberman/bomberman01.png').convert_alpha(), tamanho),
+          pygame.transform.scale(pygame.image.load('Bomberman/bomberman02.png').convert_alpha(), tamanho),
+          pygame.transform.scale(pygame.image.load('Bomberman/bomberman03.png').convert_alpha(), tamanho),
+          pygame.transform.scale(pygame.image.load('Bomberman/bomberman03.png').convert_alpha(), tamanho)
+      ]
+      self.image_index = 0
+      self.image = self.images[self.image_index]
+      self.rect = self.image.get_rect()
 
-        #Tempo de troca de animação:
-        self.tempo_animacao = 0.01
-        self.contador_tempo = 0
+      #Definindo posição inicial do jogador:
+      self.rect.topleft = posicao
 
-        #Variaveis de controle do tempo de plantar a bomba:
-        self.tempo_ultimo_plante = 0
-        self.intervalo_bomba = 3
+      #Tempo de troca de animação:
+      self.tempo_animacao = 0.01
+      self.contador_tempo = 0
 
-    @property
-    def posicao(self):
+      #Variaveis de controle do tempo de plantar a bomba:
+      self.tempo_ultimo_plante = 0
+      self.intervalo_bomba = 3
+     
+
+   @property
+   def posicao(self):
         return self.__posicao
-    @property
-    def vida(self):
+   @property
+   def vida(self):
         return self.__vida
-    @property
-    def velocidade(self):
+   @property
+   def velocidade(self):
         return self.__velocidade
-    @property
-    def raio(self):
+   @property
+   def raio(self):
         return self.__range_bomba
 
-    def movimento(self):
-        keys = pygame.key.get_pressed()
-        movimento_x = 0
-        movimento_y = 0
-        if keys[pygame.K_w]:
-            movimento_y -= self.__velocidade
-            self.image = self.images[2]
-            self.direcao = 'up'
-        if keys[pygame.K_s]:
-            movimento_y += self.__velocidade
-            self.image = self.images[0]
-            self.direcao = 'down'
-        if keys[pygame.K_d]:
-            movimento_x += self.__velocidade
-            self.image = self.images[1]
-            self.direcao = 'right'
-        if keys[pygame.K_a]:
-            movimento_x -= self.__velocidade
-            self.image = self.images[3]
-            self.direcao = 'left'
+   def movimento(self):
+     keys = pygame.key.get_pressed()
+     movimento_x = 0
+     movimento_y = 0
+     if keys[pygame.K_w]:
+         movimento_y -= self.__velocidade
+         self.image = self.images[2]
+     if keys[pygame.K_s]:
+         movimento_y += self.__velocidade
+         self.image = self.images[0]
+     if keys[pygame.K_d]:
+         movimento_x += self.__velocidade
+         self.image = self.images[1]
+     if keys[pygame.K_a]:
+         movimento_x -= self.__velocidade
+         self.image = self.images[3]
+    
+   #Movimenta o Jogador no eixo X e checa colisões:
+     self.rect.x += movimento_x
+     bloco_colidido = pygame.sprite.spritecollideany(self, self.mapa.blocos)  
+     bomba_colidida = pygame.sprite.spritecollideany(self, self.mapa.bombas) 
+     if bloco_colidido or bomba_colidida:
+         if bloco_colidido:
+             self.colisao(bloco_colidido, eixo='x')
+         if bomba_colidida:
+             self.colisao(bomba_colidida, eixo='x')
+     
+    # Movimenta o jogador no eixo Y e checa colisões
+     self.rect.y += movimento_y
+     bloco_colidido = pygame.sprite.spritecollideany(self, self.mapa.blocos)  
+     bomba_colidida = pygame.sprite.spritecollideany(self, self.mapa.bombas) 
+     if bloco_colidido or bomba_colidida:
+         if bloco_colidido:
+             self.colisao(bloco_colidido, eixo='y')
+         if bomba_colidida:
+             self.colisao(bomba_colidida, eixo='y')
 
-        #Movimenta o Jogador no eixo X e checa colisões:
-        self.rect.x += movimento_x
-        bloco_colidido = pygame.sprite.spritecollideany(self, self.mapa.blocos)  
-        bomba_colidida = pygame.sprite.spritecollideany(self, self.mapa.bombas) 
-        if bloco_colidido or bomba_colidida:
-            if bloco_colidido:
-                self.colisao(bloco_colidido, eixo='x')
-            if bomba_colidida:
-                self.colisao(bomba_colidida, eixo='x')
-        
-        # Movimenta o jogador no eixo Y e checa colisões
-        self.rect.y += movimento_y
-        bloco_colidido = pygame.sprite.spritecollideany(self, self.mapa.blocos)  
-        bomba_colidida = pygame.sprite.spritecollideany(self, self.mapa.bombas) 
-        if bloco_colidido or bomba_colidida:
-            if bloco_colidido:
-                self.colisao(bloco_colidido, eixo='y')
-            if bomba_colidida:
-                self.colisao(bomba_colidida, eixo='y')
+     self.__posicao = self.rect.topleft
 
-        self.__posicao = self.rect.topleft
+   def colisao(self, sprite, eixo):
+       for bomba in self.mapa.bombas:
+           if pygame.sprite.collide_rect(self, bomba):
+               print("Colisão com bomba detectada")
+       if eixo == 'x':
+           if self.rect.right > sprite.rect.left and self.rect.left < sprite.rect.right:
+               if self.rect.centerx < sprite.rect.centerx:
+                   self.rect.right = sprite.rect.left
+               else:
+                   self.rect.left = sprite.rect.right
+       if eixo == 'y':
+           if self.rect.bottom > sprite.rect.top and self.rect.top < sprite.rect.bottom:
+               if self.rect.centery < sprite.rect.centery:
+                   self.rect.bottom = sprite.rect.top
+               else:
+                   self.rect.top = sprite.rect.bottom
 
-    def colisao(self, sprite, eixo):
-        for bomba in self.mapa.bombas:
-            if pygame.sprite.collide_rect(self, bomba):
-                print("Colisão com bomba detectada")
-        if eixo == 'x':
-            if self.rect.right > sprite.rect.left and self.rect.left < sprite.rect.right:
-                if self.rect.centerx < sprite.rect.centerx:
-                    self.rect.right = sprite.rect.left
-                else:
-                    self.rect.left = sprite.rect.right
-        if eixo == 'y':
-            if self.rect.bottom > sprite.rect.top and self.rect.top < sprite.rect.bottom:
-                if self.rect.centery < sprite.rect.centery:
-                    self.rect.bottom = sprite.rect.top
-                else:
-                    self.rect.top = sprite.rect.bottom
 
     #Metodo para Jogador plantar a bomba, ajuste de tamanho, tempo, raio da bomba e o tempo de um plante para o outro:
-    def plantar_bomba(self, dt):
-        current_time = pygame.time.get_ticks() / 1000 #Obtem o tempo atual em segundos
-        if current_time - self.tempo_ultimo_plante >= self.intervalo_bomba:
-            if self.image == self.images[0]:
+   def plantar_bomba(self, dt):
+       current_time = pygame.time.get_ticks() / 1000 #Obtem o tempo atual em segundos
+       if current_time - self.tempo_ultimo_plante >= self.intervalo_bomba:
+           if self.image == self.images[0]:
                 bomba_pos = (self.rect.centerx - 25, (self.rect.bottom + self.rect.height // 2) - 10)
-            elif self.image == self.images[1]:  # Imagem apontando para direita
+           elif self.image == self.images[1]:  # Imagem apontando para direita
                 bomba_pos = ((self.rect.right + self.rect.width // 2) - 20, self.rect.centery - 19)
-            elif self.image == self.images[2]:  # Imagem apontando para cima
+           elif self.image == self.images[2]:  # Imagem apontando para cima
                 bomba_pos = (self.rect.centerx - 20, (self.rect.top - self.rect.height // 2) - 20)
-            elif self.image == self.images[3]:  # Imagem apontando para esquerda
+           elif self.image == self.images[3]:  # Imagem apontando para esquerda
                 bomba_pos = (self.rect.left - 40, self.rect.centery - 20)
-            else:
-                print('ue')
+           else:
+               print('ue')
+                
+        
 
-            bomba = Bomba(bomba_pos, 4.0, 15, (40, 40), self.direcao, self.mapa)
+           bomba = Bomba(bomba_pos, 4.0, 20, (40, 40), self.mapa)
 
-            self.mapa.bombas.add(bomba)
-            self.tempo_ultimo_plante = current_time #Atualiza o tempo da ultima bomba plantada
-
-    def sofrer_dano(self):
+           self.mapa.bombas.add(bomba)
+           self.tempo_ultimo_plante = current_time #Atualiza o tempo da ultima bomba plantada
+      
+   def sofrer_dano(self):
         self.__vida -= 1
         if self.__vida <= 0:
             self.morrer()
+ 
+   def morrer(self):
+       print("O jogador morreu!!")
+       self.kill()
 
-    def morrer(self):
-        print("O jogador morreu!!")
-        self.kill()
-
-    def update(self, dt):
-        self.movimento()
-        #self.animacao(dt)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            self.plantar_bomba(dt)
+       
+   def update(self, dt):
+     self.movimento() 
+     #self.animacao(dt)
+     keys = pygame.key.get_pressed()
+     if keys[pygame.K_SPACE]:
+         self.plantar_bomba(dt)
+     
